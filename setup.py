dcb14670c2f08134767a914fc04a612890977da1
zekroTJA <riho@live.de>
2019-12-13 13:31:15 +0100

    update minor version in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    version="0.1.0",
+    version="0.2.0",
     name="pytter",
     author="zekro",
     author_email="contact@zekro.de",
